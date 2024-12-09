0ba42f106d517bfb5bbb03d1b0acc86c6fefaae9
GrizztexBL <grizzlebl@jacks.sfasu.edu>
2024-12-08 19:44:39 -0600

    Changed mine value for hard difficulty

--- src/MineSweeperConstants.java
+++ src/MineSweeperConstants.java
@@ -2,5 +2,5 @@ public class MineSweeperConstants {
     public static int MENU_WIDTH = 1000, MENU_HEIGHT = 600, TILE_SIZE = 50;
     public static int EASY_ROW = 8, EASY_COL = 8, EASY_MINES = 10;
     public static int MED_ROW = 12, MED_COL = 12, MED_MINES = 30;
-    public static int HARD_ROW = 20, HARD_COL = 14, HARD_MINES = 75;
+    public static int HARD_ROW = 20, HARD_COL = 14, HARD_MINES = 55;
 }
